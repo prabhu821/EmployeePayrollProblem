0bb5de79241dca59b23dfa7bacea143ae9f53ff9
rishiraj007 <rishiraj@mounttalent.com>
2023-04-19 19:33:21 +0530

    [Prabhu]Add: Employee Payroll Problem

---
+++ EmplloyeePayrollProblem/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EmplloyeePayrollProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Payroll Problem!");
+        }
+    }
+}

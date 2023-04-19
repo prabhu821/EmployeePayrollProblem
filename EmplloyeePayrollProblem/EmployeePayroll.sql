9e98091ba31d605d640989d0bfc2712fbf653d2c
rishiraj007 <rishiraj@mounttalent.com>
2023-04-19 19:40:16 +0530

    [Prabhu]Add: Create table

--- EmplloyeePayrollProblem/EmployeePayroll.sql
+++ EmplloyeePayrollProblem/EmployeePayroll.sql
@@ -1,3 +1,7 @@
 ﻿--UC1 Ability to create a payroll service database
 create database Payroll;
 use Payroll;
+
+--UC2 Ability to create a employee payroll table in the payroll service database 
+create table  employee_payroll(Id int Identity(1,1)Primary Key, Name varchar(100), Salary float, StartDate date);
+
